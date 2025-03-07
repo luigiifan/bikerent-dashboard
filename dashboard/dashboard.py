e0298c39efc81f321894a015a8615649db291f5d
Luigi Ifan <luigiifan5@gmail.com>
2025-03-07 22:30:28 +0700

    Update dashboard.py

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -69,7 +69,7 @@ fig, ax = plt.subplots()
 sns.scatterplot(data=filtered_df, x='atemp', y='cnt', hue='workingday', alpha=0.5, ax=ax, palette={0: "red", 1: "green"})
 ax.set_xlabel("Suhu yang Dirasakan")
 ax.set_ylabel("Jumlah Penyewaan Sepeda")
-ax.set_title("Korelasi Suhu vs Penyewaan (Hari Kerja vs Akhir Pekan)")
+ax.set_title("Korelasi Suhu vs Penyewaan")
 ax.legend(title="Hari Kerja", labels=["Akhir Pekan", "Hari Kerja"])
 st.pyplot(fig)
 
