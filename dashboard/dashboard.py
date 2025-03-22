d3a35c8d5ec97e9a08e45d99cf6ea509162128f6
Luigi Ifan <luigiifan5@gmail.com>
2025-03-22 11:35:07 +0700

    Update dashboard.py

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -53,8 +53,8 @@ st.subheader("🌎 Pengaruh Cuaca terhadap Penyewaan Sepeda")
 weather_counts = filtered_df.groupby("weathersit")["cnt"].sum().reset_index()
 st.markdown(
     f"""
-    <div style="background-color: #f4f4f4; padding: 20px; border-radius: 10px;">
-        <h4 style="color: #808080;">Total Penyewaan Berdasarkan Cuaca:</h4>
+    <div style="background-color: #00000; padding: 20px; border-radius: 10px;">
+        <h4 style="color: #333;">Total Penyewaan Berdasarkan Cuaca:</h4>
         <ul style="list-style-type: none; padding-left: 0;">
             <li><b>☀️ Baik (Cerah) --> </b> {weather_counts[weather_counts['weathersit'] == 'Baik']['cnt'].values[0]:,.0f} sepeda</li>
             <li><b>⛅ Sedang (Berawan) --> </b> {weather_counts[weather_counts['weathersit'] == 'Sedang']['cnt'].values[0]:,.0f} sepeda</li>
